f8210186e284b03dbfbaa8d88c8d1e47a2585b54
berkin9 <berkinakbiyik@hotmail.com>
2025-04-25 11:34:03 +0300

    add missing tunings

---
+++ src/repsy-storage-api/src/main/java/io/repsy/RepsyStorageApiApplication.java
@@ -0,0 +1,11 @@
+package io.repsy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepsyStorageApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RepsyStorageApiApplication.class, args);
+    }
+}
